93947ccc09f12ab684019b96463fb644337b898c
Al <blacksuperbat@gmail.com>
2025-01-22 23:42:59 +0800

    update

--- IT/it.php
+++ IT/it.php
@@ -141,6 +141,7 @@
     <!--END::PROFILE FUNCTION-->
     <!-- START::ARCHIVE FUNCTION -->
     <script src="crud-ajax/masterlists/archive-users.js"></script>
+    <!-- <script src="crud-ajax/masterlists/archive-users.js"></script> -->
     <!-- END::ARCHIVE FUNCTION -->
 </body>
 </html>
