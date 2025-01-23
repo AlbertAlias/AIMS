10781639059fcaa548cea9a5db975dbb39f105ca
Al <blacksuperbat@gmail.com>
2025-01-23 19:16:07 +0800

    Update students-lists.php

--- Coordinator/pages/students-lists.php
+++ Coordinator/pages/students-lists.php
@@ -115,7 +115,6 @@
                 </form>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                 <button type="button" class="btn btn-primary" id="saveHoursButton">Set Hours</button>
             </div>
         </div>
